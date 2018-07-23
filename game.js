9212bf5a7f6045ea25fc72c01212171fc7c7316e
Joe Anderson <joejbajoe@gmail.com>
2018-07-23 15:04:09 +0100

    Extract player to class

--- game.js
+++ game.js
@@ -14,12 +14,9 @@ var level = {
     game.physics.startSystem(Phaser.Physics.P2JS);
     game.stage.backgroundColor = 0xffffff;
 
-    player = game.add.sprite(400, 300, 'player');
-    var ratio = 870 / 298;
-    player.width = 50;
-    player.height = player.width * ratio;
+    player = new Player();
+    player.init(game);
 
-    game.physics.p2.enable(player);
     game.physics.p2.gravity.y = 100;
   }
 }
---
+++ player.js
@@ -0,0 +1,10 @@
+class Player { 
+  init(game) {
+    this.sprite = game.add.sprite(400, 300, 'player');
+    var ratio = 870 / 298;
+    this.sprite.width = 50;
+    this.sprite.height = this.sprite.width * ratio;
+
+    game.physics.p2.enable(this.sprite);
+  }
+}
