f5ef414a99ae935e8db5d846821897e2c8664020
Joe Anderson <joejbajoe@gmail.com>
2018-07-25 15:13:19 +0100

    Fix misaligned platforms

--- platform.js
+++ platform.js
@@ -1,8 +1,8 @@
 class Platform {
   constructor(x, y, length) {
-    this.sprite = platforms.create(x + ( length / 2 ), y, 'platform');
-    this.sprite.anchor.setTo(0,0);
+    this.sprite = platforms.create(x, y, 'platform');
     this.sprite.body.setRectangle(length, 20);
+    this.sprite.anchor.setTo(0,0);
     this.sprite.width = length;
     this.sprite.height = 20;
 
