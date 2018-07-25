109c9fb71254e015d72b7dcaa05eab5baab613e8
Joe Anderson <joejbajoe@gmail.com>
2018-07-25 15:34:01 +0100

    Fix misaligned platforms again

--- platform.js
+++ platform.js
@@ -1,10 +1,9 @@
 class Platform {
   constructor(x, y, length) {
     this.sprite = platforms.create(x, y, 'platform');
-    this.sprite.body.setRectangle(length, 20);
-    this.sprite.anchor.setTo(0,0);
     this.sprite.width = length;
     this.sprite.height = 20;
+    this.sprite.body.setRectangleFromSprite(this.sprite);
 
     this.sprite.body.static = true;
     this.sprite.body.setCollisionGroup( platformCollisionGroup );
