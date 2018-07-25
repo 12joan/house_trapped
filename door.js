cd7e2da433b8beb33799d6010cf42650ac2fa47a
MaxHearnden <max@hearnden.org.uk>
2018-07-25 22:27:46 +0100

    fixed part of level and door

--- door.js
+++ door.js
@@ -1,7 +1,7 @@
 class Door {
   constructor(x, y, d) {
     var destination = d;
-    this.sprite = doors.create(x, y, 'door');
+    this.sprite = doors.create(x+50, y-110, 'door');
     this.sprite.body.setRectangle(this.sprite.width, this.sprite.height);
 
     this.sprite.body.static = true;
